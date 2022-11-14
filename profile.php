301c5ce06f609059826a3478aa733d258ac46e93
Lastpeace <caesarios722@gmail.com>
2022-11-14 12:09:18 +0700

    Profile

--- profile.php
+++ profile.php
@@ -3,31 +3,27 @@ session_start();
 include 'connect.php';
 
 $_SESSION['username'] = $row['username'];
-$sql = "SELECT * FROM `user` WHERE username='$username' and password='$password'";
+$_SESSION['password'] = $row['password'];
+$sql = "SELECT * FROM user";
 $query = mysqli_query($conn,$sql);
     if($row=mysqli_fetch_array($query)){
-    echo"<tr>
-        <td>$row[id]</td>
-        <td>$row[username]</td>
-        <td>$row[aboutme]</td>
-        <td>$row[bio]</td>
-        <td>
-    </tr>";
+    echo "hai";
     }else{}
 ?>
 
 <div class="kotakan">
             <header class="profile">
-                <div class="hero">
-                    <h1 class="username"><?php echo $_SESSION['username']?></h1>
+                <div class="atas">
+                    <h1 class="username"><?php echo $row['username']?></h1>
                 </div>
-                <div class="features feature-1">
-                    <h4 class="price">Dexazor Esport</h4>
-                    <p class="item">Pro Player</p>
+                <div class="Folowers">
+                <p class="followers">Followers</p>
+                <p class="following">Following</p>
+                <p class="jumlahpost">Jumlah Post</p>
+
                 </div>
-                <div class="features feature-2">
-                <h4 class="price">HMIF</h4>
-                <p class="item">PSDM</p>
+                <div class="aboutme">
+                    <p class="aboutme1"><?php $sql="SELECT * from user where aboutme="?></p>
                 </div>
             </header>
 </div>

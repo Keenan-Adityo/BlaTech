8b7232c6e287bcd3aabca0a0c25e8eeda84c0854
Keenan-Adityo <keenanadityo167@gmail.com>
2022-11-14 20:15:12 +0700

    add includes for navigation

--- home.php
+++ home.php
@@ -1,21 +1,18 @@
 <?php 
     session_start();
+    include 'includes/navigation.php';
 ?>
 
 <html lang="en">
 <head>
     <title>Blatech</title>
-    <link rel="stylesheet" href="style.css">
+    
     <!-- CSS only -->
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div id="mySidenav" class="sidenav">
-    <img src="assets/logo.png" alt="" class="logo">
-    <a href="home.php">Home</a>
-    <a href="create_post.php">[+] (create post)</a>
-    <a href="profile.php">Profil</a>
-    <a href="logout.php">Log Out</a>
-    </div>
+    <?php  ?>
+    
 </body>
 </html>

b279a3ddb6112da849b185aa73f4cffea434e1f5
Keenan-Adityo <keenanadityo167@gmail.com>
2022-11-14 12:15:38 +0700

    add route navigationn

--- home.php
+++ home.php
@@ -10,13 +10,12 @@
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"> -->
 </head>
 <body>
-
     <div id="mySidenav" class="sidenav">
-            <img src="img/Blatech.png" alt="" class="logo">
-    <a href="#">Home</a>
-    <a href="#">[+] (create post)</a>
-    <a href="#">Profil</a>
-    <a href="Logout">Log Out</a>
+    <img src="img/logo.png" alt="" class="logo">
+    <a href="home.php">Home</a>
+    <a href="create_post.php">[+] (create post)</a>
+    <a href="profile.php">Profil</a>
+    <a href="logout.php">Log Out</a>
     </div>
 </body>
 </html>

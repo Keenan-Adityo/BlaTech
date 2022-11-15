adfc04e3110da1b14d1fe5a5b84802b9f0c4bd70
Lastpeace <caesarios722@gmail.com>
2022-11-15 18:59:51 +0700

    mempercantik bagian login

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 session_start();
 include 'connect.php';
-
 $username = $_POST['username'];
 $password = $_POST['password'];
 $login =$_POST['login'];
-
 $sql = "SELECT * FROM `user` WHERE username='$username' and password='$password'";
 $query = mysqli_query($conn,$sql);
 $row = mysqli_fetch_array($query);
@@ -25,24 +23,32 @@ if(isset($_POST['login'])){
 
 ?>
 
-
+<link rel="stylesheet" href="index.css">
+<div class="wrapper">
+    <div class="get">
+        <p id="p1" > <b>BlaTech</b><br>Cari sekarang juga.</p>
+        <p id="p2"><input type="submit" value="Dapatkan"></p>
+    </div>
 <form name="form" method="post">
 
 <img src="./assets/logo.png"  width="200px" height="200px" style=" display: block; margin-left: auto;margin-right: auto;" >
 <table align="center" border="0">
 	<tr>
-		<td><input name='username' type='text' placeholder="username"></td>
+		<td><input name='username' type='text' placeholder="username" class="input"></td>
 	</tr>
 	<br>
 	<tr>
-		<td><input name='password' type='password' placeholder="password"></td>
+		<td><input name='password' type='password' placeholder="password" class="input"></td>
 	</tr>
 	<br>
 	<tr>
 		<td><input name='login' type='submit' value='Log in'></td>
 	</tr>
 	
-</table>
+</table><div class="signup">
 <p align="center">Tidak Punya Akun di Blatech? <a href="signup.php">Daftar!</a></p>
+</div>
+</form>
+</div>
 
 <html>

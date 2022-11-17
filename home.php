73260a5d1e311c43617dc63612a4fd73b8ce9704
Keenan-Adityo <keenanadityo167@gmail.com>
2022-11-17 16:08:54 +0700

    UI home.php

--- home.php
+++ home.php
@@ -6,10 +6,11 @@
 <html lang="en">
 <head>
     <title>Blatech</title>
-    
+    <!-- icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
     <!-- <div class="card">
@@ -22,7 +23,9 @@
     <div class="card border-success mb-3" style="max-width: 35rem; margin: auto;">
         <div class="card-header">
             <div class="d-flex flex-row">
-                <div class="p-2">pfp</div>
+                <div class="p-2">
+                    <img src="assets/Ammar.jpg" alt="avatar" class="avatar">
+                </div>
                 <div class="p-2 flex-fill">username</div>
             </div>
         </div>
@@ -31,17 +34,20 @@
             <div class="card-body">
             </div>
         </div>
-        
         <div class="card-footer">
              <div class="d-flex flex-column">
                 <div class="d-flex flex-row">
-                    <div class="p-2">like</div>
-                    <div class="p-2">comment</div>
+                    <div class="p-2"><i class="bi bi-heart"></i></div>
+                    <div class="p-2"><i class="bi bi-chat"></i></div>
                 </div>
-                <div class="d-flex flex-column">
-                    <div class="p-2">komen 1</div>
-                    <div class="p-2">komen 2</div>
-                </div>
+                <?php
+                $idx = 10;
+                echo "<p><b>username</b> deskripsi posting</p>";
+                // view comment masuk ke modal 
+                echo "<p>View all 0 comments</p>";
+                echo "<p><b>username</b> 1 top komen</p>";
+                ?>
+                
              </div>
         </div>
     </div>

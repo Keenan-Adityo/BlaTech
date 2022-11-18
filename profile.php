16dd0d6934039e37e9d9e837b4495998d7e7260d
Lastpeace <caesarios722@gmail.com>
2022-11-18 13:55:31 +0700

    Profile dalam proses

--- profile.php
+++ profile.php
@@ -3,13 +3,14 @@ session_start();
 include 'connect.php';
 
 $id = $_SESSION['id'];
-
 $sql = "select * from user where id='$id'";
 $query = mysqli_query($conn,$sql);
     if($row=mysqli_fetch_array($query)){
         
     }else{}
 ?>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
 <div class="bagianatas">
             <header class="profile">
@@ -28,8 +29,14 @@ $query = mysqli_query($conn,$sql);
                     <p class="aboutme1"><?php $row['bio']?></p>
                 </div>
             </header>
-
-            <td><?PHP echo"<img src='$row[foto]' width='300' height='200'>"?></td>
+    <div class="container text-center">
+        <div class="row row-cols-auto">
+            <div class="col"><td><?PHP echo"<img src= 'assets/profile_picture/"; echo $row['foto'];  echo "' width='300' height='200'>"?></td></div>
+            <div class="col"><td><?PHP echo"<img src= 'assets/profile_picture/"; echo $row['foto'];  echo "' width='300' height='200'>"?></td></div>
+            <div class="col"><td><?PHP echo"<img src= 'assets/profile_picture/"; echo $row['foto'];  echo "' width='300' height='200'>"?></td></div>
+            <div class="col"><td><?PHP echo"<img src= 'assets/profile_picture/"; echo $row['foto'];  echo "' width='300' height='200'>"?></td></div>
+            </div>
+    </div>
 </div>
 	
 

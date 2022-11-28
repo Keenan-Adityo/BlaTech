ec05e4860ff0b7d214db507b609a9eaf31552780
Keenan-Adityo <keenanadityo167@gmail.com>
2022-11-28 19:59:51 +0700

    add detailed post section in modal

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <?php 
     session_start();
     include 'includes/navigation.php';
-    include 'includes/comment_modal.php';
+    // include 'includes/comment_modal.php';
     include 'isLogged.php';
     include 'connect.php';
     $id = $_SESSION['id'];
@@ -10,7 +10,7 @@
     // cek follow
     
     // cek postnya follow tampilin
-
+    // if(isset($_POST))
 ?>
 
 <html lang="en">
@@ -43,31 +43,66 @@
                         echo 
                         "
                         <div class='card' style='max-width: 35rem;'>  
+                            <div class='card-header'>
+                            <div class='d-flex flex-row'>
+                                <div class='p-2'>
+                                     <img src='assets/profile_picture/".  $o_user['foto']. "' alt='avatar' class='avatar' style='width: 25px; height: 25px;'>
+                                </div>
+                                <div class='p-2 flex-fill'>".  $o_user['nama'].  "</div>
+                                </div>
+                            </div>
                             <div class='card-body'>
-                                <div class='d-flex flex-row'>
-                                    <div class='p-2'>
-                                        <img src='assets/profile_picture/".  $o_user['foto']. "' alt='avatar' class='avatar' style='width: 25px; height: 25px;'>
-                                    </div>
-                                    <div class='p-2 flex-fill'>".  $o_user['nama'].  "</div>
-                                </div>
-                                <img src='assets/feed_post/".  $feed['foto_feedpost'].  "' alt='blatech' class='card-img'>
-                                <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#staticBackdrop'>
-                                    <i class='bi bi-chat'></i>
-                                </button>
-                                
-                                </div>
+                                <img src='assets/feed_post/".  $feed['foto_feedpost'].  "' alt='blatech' class='card-img'>  
+                            </div>
                             <div class='card-footer'>
-                            
                                 <div class='d-flex flex-column'>
                                     <div class='d-flex flex-row'>
                                         <div class='p-2'><i class='bi bi-heart'></i></div>
                                         <div class='p-2'>
                                         
-                                            <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#staticBackdrop'>
+                                            <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#comment_". $feed['id_feedpost']. "'>
                                                 <i class='bi bi-chat'>
                                                 </i>
                                             </button>
-                                        
+                                            <div class='modal fade modal-xl' id='comment_". $feed['id_feedpost']. "' data-bs-backdrop='static' data-bs-keyboard='false' tabindex='-1' aria-labelledby='staticBackdropLabel' aria-hidden='true'>
+                                                <div class='modal-dialog modal-dialog-centered'>
+                                                    <div class='modal-content'>
+                                                    <div class='modal-body'>
+                                                        <div class='d-flex flex-row'>
+                                                            <div class='p-2'>
+                                                                <img src='assets/feed_post/" . $feed['foto_feedpost'] . "' width='500' height'500'>
+                                                            </div>  
+                                                            <div class='p-2 flex-fill'>
+                                                                <div class='d-flex flex-column'>
+                                                                    <div class='d-flex flex-row'>
+                                                                        <div class='p-2'>
+                                                                            <img src='assets/profile_picture/".  $o_user['foto']. "' alt='avatar' class='avatar' style='width: 25px; height: 25px;'>
+                                                                        </div>
+                                                                        <div class='p-2 flex-fill'>".  $o_user['nama'].  "</div>
+                                                                        <div class='p-2'><button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button></div>
+                                                                    </div>
+                                                                    <div style='overflow-y: scroll; height:350px;'>
+                                                                    
+                                                                    </div>
+                                                                    <div class='p-2'>
+                                                                        <div class='d-flex flex-row'>
+                                                                            <div class='p-2'><i class='bi bi-heart'></i></div>
+                                                                            <div class='p-2'>
+                                                                                <i class='bi bi-chat'>
+                                                                                </i>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class='p-2'>
+                                                                        <input type='text' name='komen'>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                     <p><b>".  $o_user['username'].  "</b> ".  $feed['description'].  "</p>

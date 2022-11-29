1e08586001699ac4f73bab38719796e3e8c981c5
Lastpeace <caesarios722@gmail.com>
2022-11-29 13:41:16 +0700

    UDAH SELESAI BAGIAN EDIT PROFILE

--- editprofile.php
+++ editprofile.php
@@ -1,55 +1,39 @@
 <?php
 session_start();
 include 'connect.php';
-//include 'isSigning.php';
 
-$id = $_SESSION['id'];
-$username = $_SESSION['username'];
-$password = $_SESSION['password'];
-$nama = $_SESSION['nama'];
-$email = $_SESSION['email'];
-$bio = $_POST['bio'];
-$foto = $_POST['foto'];
+
+
 
 $filename = $_FILES["foto"]["name"];
 $tempname = $_FILES["foto"]["tmp_name"];
 $folder = "./assets/profile_picture/" . $filename;
-
-$sql="update user set username='$username', nama='$nama', foto='$foto',bio='$bio' where id='$id'";
-$id2= $row['id'];
-
-if(isset($_POST['simpan'])){
-	if($row['username']==$username && $row['password']==$password){
-		$_SESSION['id'] = $row['id'];
-
-		?>
-		<script> alert("Berhasil disimpan"); document.location='profile.php';</script>
-		<?php
-		$_SESSION['simpan'] = true;
-	}else{
-			?>
-		<script> alert("Ngga jadi edit"); document.location='index.php';</script>
-		<?php
+if($id = $_SESSION['id']){
+	$sesi = $id;
+	} else if($_SESSION[ 'admin']) {
+	$sesi = $_SESSION('admin');
 	}
-}
-?>
+	$query = mysqli_query($conn,"SELECT * FROM user WHERE id = '$id'");
+	$data= mysqli_fetch_array($query);
 
-<form name="form" method="post">
+?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<!-- <form name="form" method="post">
 <table align="center" border="0">
 <tr>
 		<td>Username</td>
         <td>:</td>
-        <td><input type="text" name="username" id="username"  class="editprofile" ></input></td>
+        <td><input type="text" name="username2" id="username2"  class="editprofile" value="<?PHP echo$data['username'] ?>"></input></td>
 	</tr>
     <tr>
 		<td>Nama</td>
         <td>:</td>
-        <td><input name="nama" id="nama" class="editprofile" ></input>
+        <td><input name="nama2" id="nama2" class="editprofile" value="<?PHP echo$data['nama'] ?>"></input>
 	</tr>
     <tr>
 		<td>Bio</td>
         <td>:</td>
-        <td><textarea name="bio" id="bio" maxlength="100" class="editprofile" ></textarea></td>
+        <td><input name="bio2" id="bio2" maxlength="100" class="editprofile" value="<?PHP echo$data['bio'] ?>"></input></td>
 	</tr>
     </br>
     <tr>
@@ -65,4 +49,47 @@ if(isset($_POST['simpan'])){
 	</tr>
 </table>
 
+</form> -->
+<form class="" name="form" method="post" >
+  <div class="form-row">
+    <div class="col-md-4 mb-3">
+      <label for="validationCustom01">Nama Lengkap</label>
+      <input type="text" class="form-control" name="nama2" id="validationCustom01" placeholder="Nama Lengkap" value="<?PHP echo$data['nama'] ?>" required>
+    </div>
+    <div class="col-md-4 mb-3">
+      <label for="validationCustomUsername">Username</label>
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="inputGroupPrepend">@</span>
+        </div>
+        <input type="text" class="form-control" name="username2" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend" value="<?PHP echo$data['username'] ?>"required>
+      </div>
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="col-md-6 mb-3">
+      <label for="validationCustom03">Bio</label>
+      <input type="text" class="form-control" name="bio2" id="validationCustom03" value="<?PHP echo$data['bio'] ?>">
+    </div>
+    <div class="col-md-3 mb-3">
+      <label for="validationCustom04">PASSWORD</label>
+      <input type="text" class="form-control" name="password2" id="validationCustom04" placeholder="PASSWORD" value="<?PHP echo$data['password'] ?>" required>
+    </div>
+    <div class="col-md-3 mb-3">
+      <label for="validationCustom05">Email</label>
+      <input type="email" class="form-control" name="email2" id="validationCustom05" placeholder="email" value="<?PHP echo$data['email'] ?>"required>
+    </div>
+  </div>
+  <input class="btn btn-primary" type="submit" name="simpan" value="Simpan"></input> <input  class="btn btn-primary" type="button" onclick="location.href='Profile.php';" value="Kembali" />
 </form>
+<?php
+if($_POST['simpan']){
+	$username = ($_POST['username2']);
+	$nama = ($_POST['nama2']);
+	$bio = ($_POST['bio2']);
+	$password = ($_POST['password2']);
+	$email = ($_POST['email2']);
+	mysqli_query($conn,"update user set username ='$username', nama = '$nama', bio='$bio',password ='$password', email='$email' where id = $sesi");
+	header("location: profile.php");
+}
+?>
--- profile.php
+++ profile.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+ob_start();
 include 'connect.php';
 include 'isloggin.php';
 include 'connect.php';
@@ -8,59 +9,71 @@ $id = $_SESSION['id'];
     $sql ="select * from user where id = '$id'";
 	$query = mysqli_query($conn,$sql);
     $user = mysqli_fetch_array($query);
+    $followQuery = mysqli_query($conn, "select * from follow where id_follow = '$id'");
+    while($follow = mysqli_fetch_array($followQuery)) {
+      $id_follow = $follow['id_follow'];
+    }
 ?>
 <link rel="stylesheet" href="profile.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
 
 <body>
-<div class="row">
+<div class="bagianprofile">
             <div class="bagianatas d-flex">
               <div class="bagianfoto">
-                    <?PHP echo"<img src= 'assets/profile_picture/"; echo $user['foto'];  echo "' width='200' height='200'>"?>
+                    <?PHP echo"<img src= 'assets/profile_picture/" ; echo $user['foto'];  echo "' width='200px' height='200px' >"?>
               </div>
               <section class="palingatas">
-                  <h2 class="usernamenyaini"><?php echo $user['username']?></h2>
+                  
                 <?php
                 $ini = mysqli_query($conn, "select * from user where id = '$id'");
                 if($userini = mysqli_fetch_array($ini)){
                   ?>
-                    <div class="editprofile" style="flex: 1 0 auto";>
-                    <button class="edit"><a class="kangedit" href="editprofile.php" style="text-decoration: none; color:black;">Edit Profile</a></button>
+                    <div class="editprofile" style="display:flex;">
+                    <h2 class="usernamenyaini" style="margin-right: 50px;"><?php echo $user['username']?></h2>
+                    <button class="edit" style="border-radius=50px"><a class="kangedit" href="editprofile.php" style="text-decoration: none; color:black;">Edit Profile</a></button>
                   </div>
                 <?PHP
                 }else{
                   ?>
                   <div class='follow d-flex pt-1'>
                   <button type='button' class='follow btn-primary flex-grow-1'>Follow</button>
-                </div>";
+                </div>
                   <?PHP                   
                 }
                 ?>
-                <ul class="d-flex">
-                  <li class="px-4">
-                    <div class="post">
-                      <span class="bagianfollowerpost"> 4 <span>Post
+                <ul class="bagianfollowerpost d-flex">
+                  <li class="postutama">
+                    <div class="post2">
+                      <span class="post"> 4 <span>Post 
                       </div>
                   </li>
-                  <li>
-                    <div class="following">4 Following</div>
+                  <li class="followingutama">
+                    <div class="following2">
+                      <span class="following"> 4 <span>Following 
+                    </div>
                   </li>
-                  <li class="px-4">
-                    <div class="followers"> 9 Followers</div>
+                  <li class="followersutama">
+                    <div class="followers2">
+                      <span class="followers"> 4 <span>Follower 
+                    </div>
                     </li>
                 </ul>
-                <p class="mb-1 pb-0" style="color: #2b2a2a;"><?php echo $user["nama"]; ?></p>
+                <br>
+                <div>
+                <span class="mb-1 pb-0" style="color: #2b2a2a;"><?php echo $user["nama"]; ?></span>
+                </div>
+                <br>
                 <p class="mb-2 pb-1" style="color: #2b2a2a;"><?php echo $user["bio"]; ?></p>
               </section>
             </div>
             <style>
               .bagianfoto{
-                margin-bottom: 50px
+                margin-bottom: 50px;
+                border-radius: 50px;
               }
-              .row{
-                align
-              }
+              
 
               ul {
                 list-style-type: none;
@@ -68,12 +81,15 @@ $id = $_SESSION['id'];
                padding: 0;
               }
 
-              .bagianfollowerfoto{
+              .bagianfollowerpost{
                 font-weight: 600;
               }
-              .post{
-                display: block;
+
+              li{
+                margin-right: 40px;
               }
+
+              
             </style>
           
     <div class="container text-center">
